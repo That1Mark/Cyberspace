3921662146dacef44bd7b3b63068a25c0c0e40ef
Mark Reiter <markr1197@gmail.com>
2014-06-11 16:35:07 -0500

    First

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cyberspace::Application.config.secret_key_base = '9ac3ca9163751b6e7ac6f2b34f6af66dc0d8a5cbc69479068476c5f13d9cbab8cfc3ddaaa025f5c7d880c50102b1d3f846594e520ef67b738794385cc5963203'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cyberspace::Application.config.session_store :cookie_store, key: '_cyberspace_session'
